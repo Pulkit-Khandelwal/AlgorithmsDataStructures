22ca95436d8276de0a37ed7ae807836179471e98
Pulkit Khandelwal <kpulkit95@gmail.com>
2018-05-05 16:47:24 -0400

    Add files via upload

--- Sort/sort.py
+++ Sort/sort.py
@@ -57,12 +57,138 @@ class Sort(object):
         quicksort(self.data, start, end)
         return self.data
 
+    def merge_sort(self):
+        """
+        Merge Sort
+        """
+
+        def merge(A, L, R):
+            nL = len(L)
+            nR = len(R)
+            i = 0
+            j = 0
+            k = 0
+            while i < nL and j < nR:
+                if L[i] <= R[j]:
+                    A[k] = L[i]
+                    i += 1
+                else:
+                    A[k] = R[j]
+                    j += 1
+                k += 1
+
+            while i < nL:
+                A[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < nR:
+                A[k] = R[j]
+                j += 1
+                k += 1
+
+        def mergesort(A):
+            nA = len(A)
+            if nA < 2:
+                return
+
+            mid = int(nA / 2)
+            L = A[:mid]
+            R = A[mid:]
+
+            mergesort(L)
+            mergesort(R)
+            merge(A, L, R)
+
+        mergesort(self.data)
+        return self.data
+
+    def countingsort(self, data):
+        """
+        Works for integers between the range 0 and k
+        """
+        A = data
+        B = [0] * len(A)
+        k = max(A)
+        C = [0] * int(k+1)
+
+        for i in range(len(A)):
+            C[A[i]] = C[A[i]] + 1
+
+        for i in range(1, len(C)):
+            C[i] = C[i] + C[i-1]
+
+        for i in reversed(range(0, len(A))):
+            B[C[A[i]]-1] = A[i]  # This -1 is because of the Python indexing notation
+            C[A[i]] = C[A[i]] - 1
+
+        return B
+
+    def radix(self, d):
+        """
+        d: the highest order digit
+        For example if the 456 has order = 3
+        Using counting sort
+        Alternate: Hashmap and LinkedList
+        """
+
+        def countsort(data, arr):
+            """
+            Works for integers between the range 0 and k.
+            Note that this count sort is very slightly modified to accomodate
+            Radix Sort
+            """
+            A = data
+            B = [0] * len(A)
+            k = max(A)
+            C = [0] * int(k + 1)
+            arr2 = [0] * len(A)
+
+            for i in range(len(A)):
+                C[A[i]] = C[A[i]] + 1
+
+            for i in range(1, len(C)):
+                C[i] = C[i] + C[i - 1]
+
+            for i in reversed(range(0, len(A))):
+                B[C[A[i]] - 1] = A[i]  # This -1 is because of the Python indexing notation
+                arr2[C[A[i]] - 1] = arr[i]
+                C[A[i]] = C[A[i]] - 1
+
+            return arr2
+
+        A = self.data
+        B = [0] * len(A) # for temporarily storing the integers at significant bits
+
+        mod_value = 10
+        div_value = 1
+
+        for i in range(1, d+1):
+            B = [a % mod_value for a in A]
+            B = [b / div_value for b in B]
+
+            B = countsort(B, A)
+
+            A=B
+            mod_value *= 10
+            div_value *= 10
+
+        return B
+
+    def BucketSort(self):
+        """
+        Slightly modified Bucker Sort. This is unlike the algorithm given in CLRS
+        """
+        pass
 
 if __name__ == '__main__':
-    data = [8,3,6,8,3,0,13,-1,4,1]
+    data = [17,44,34,99,23,3333,2198,54,12,93,54,777,34,6] #6,83,7,85,3,393,276,34,142,98
     sort = Sort(data)
     length_data = len(data)
     #sorted_data = sort.selection()
     #sorted_data = sort.insertion()
-    sorted_data = sort.quick(start=0, end=length_data-1)
+    #sorted_data = sort.quick(start=0, end=length_data-1)
+    #sorted_data = sort.merge_sort()
+    #sorted_data = sort.countingsort(data)
+    sorted_data = sort.radix(d=4)
     print(sorted_data)
